5ff11c5401e3ad5a68a350c4257f95437a561354
Marvin <marvin.zwolsman@gmail.com>
2018-06-16 00:22:00 +0200

    Initial commit

---
+++ src/main/kotlin/com/s63d/routescraper/RouteScraperApplication.kt
@@ -0,0 +1,11 @@
+package com.s63d.routescraper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RouteScraperApplication
+
+fun main(args: Array<String>) {
+    runApplication<RouteScraperApplication>(*args)
+}
